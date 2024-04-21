bde58cfca236ed97ea54d0168ea66b3edf497145
Lukas Jacon Barboza <lukas_jacon@hotmail.com>
2024-04-21 15:15:17 -0300

    Print com os predicts de 3x...5x

--- gerador.py
+++ gerador.py
@@ -27,4 +27,3 @@ def agora():
 def dif_time(a, b):
     #return b.second - a.second
     return a - b
-
--- grafico.py
+++ grafico.py
@@ -3,21 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 
+
 # Define a função linear que queremos ajustar aos dados.
 def func(x, a, b):
     return a * x + b
 
+
 # Carrega os dados do arquivo Excel
-df = pd.read_excel('dados_ASC_100000.xlsx')
-#df = pd.read_excel('dados_DSC_100000.xlsx')
-#df = pd.read_excel('dados_RNG_100000.xlsx')
-#f = pd.read_excel('dados_ASC_100.xlsx')
-#df = pd.read_excel('dados_DSC_100.xlsx')
-#df = pd.read_excel('dados_RNG_100.xlsx')
-
-
+df = pd.read_excel('dados_ASC_100.xlsx')
 # Definir o tamanho do gráfico
 plt.figure(figsize=(12, 8))
+
 
 # Para cada algoritmo
 markers = ['o', 'v', '^', '<', '>', 's', 'p', '*']  # Diferentes marcadores para cada algoritmo
@@ -38,14 +34,21 @@ for i, column in enumerate(df.columns[:-1]):  # Ignorar a coluna 'TAM'
     y = np.append(y, prediction_y)
 
     # Criar o gráfico
-    plt.scatter(x[:-1], y[:-1], label=f'{column} Dados', marker=markers[i % len(markers)])  # Plot da previsão
-    plt.scatter(prediction_x, prediction_y, label=f'{column} Previsão', marker=markers[i % len(markers)])  # Plot do ponto previsto
+    plt.scatter(x[:-1], y[:-1], label=f'{column} Dados', marker=markers[i % len(markers)])
+    plt.scatter(prediction_x, prediction_y, label=f'{column} Previsão', marker=markers[i % len(markers)])
     plt.plot(x, func(x, *popt), label=f'{column} Ajuste: y = {popt[0]:.4f}x + {popt[1]:.2f}')
+
+    # Print da predição do tempo para um tamanho de 3x, 4x e 5x do dados
+    for j in range(3, 6):
+        prediction_x_j = j * max(x)
+        prediction_y_j = func(prediction_x_j, *popt)
+        print(f'Predição do tempo em Milissegundos para um tamanho {j}x dos dados {column}: {prediction_y_j}')
+
 
 # Configuração e exibição do gráfico
 plt.xlabel('Tamanho da Entrada')
 plt.ylabel('Tempo de Execução')
-plt.title('Gráfico de Tendência para Algoritmos de Sort em ordem Randômica')
+plt.title('Gráfico de Tendência para Algoritmos de Sort em ordem Ascendente')
 plt.grid(True)
 plt.legend(loc='best', fontsize='small')
 plt.show()
